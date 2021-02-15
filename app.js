77c79c250319e15df10b02c1be1bda67e6c12690
Mojit33 <mz_mojtaba@yahoo.com>
2021-02-15 12:07:04 +0100

    Campground New & Create

--- app.js
+++ app.js
@@ -22,15 +22,24 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.send('HELLO FROM WELP CAMP!!!')
-})
+});
 
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
 })
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground});
+});
 
 app.listen (3000, () => {
     console.log('Serving on port 3000')
-})
+});
 
